cabddfc83ee6970e5e60994c658db5505c280b3e
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-02-02 12:48:05 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ALLOW_MISSING_DEPENDENCIES:= true
 
 # Architecture
 TARGET_ARCH := arm64
