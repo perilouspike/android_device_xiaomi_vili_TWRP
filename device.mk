4e5bdfd39d725aa3cda4dcf7da6425884c0749bb
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-02-05 01:13:29 +0530

    Update device.mk
    Commented some flags..

--- device.mk
+++ device.mk
@@ -12,43 +12,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Configure base.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+DEVICE_PATH := device/xiaomi/vili
 
-# Configure core_64_bit_only.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+# Dynamic partition stuff
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Configure gsi_keys.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#PRODUCT_PACKAGES += \
+    #bootctrl.xiaomi_sm8350.recovery \
+    #android.hardware.boot@1.1-impl-qti.recovery
 
-# Configure Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# Configure SDCard replacement functionality
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Configure twrp
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8350.recovery \
-    android.hardware.boot@1.1-impl-qti.recovery
-
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
+#PRODUCT_HOST_PACKAGES += \
+    #libandroidicu
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 30
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 31
 
 # Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+#PRODUCT_SOONG_NAMESPACES += \
+    #$(DEVICE_PATH)
 
 # otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
