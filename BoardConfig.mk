217be9753f9091996b6ce458e655110d870f7c77
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-02-04 16:34:27 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-DEVICE_PATH := device/Xiaomi/vili
+DEVICE_PATH := device/xiaomi/vili
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
