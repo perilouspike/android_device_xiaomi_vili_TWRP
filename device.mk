efb11555e6efbd62898b2e0610cc1ed61db5c93f
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-02-05 13:24:29 +0530

    Update device.mk
    Fixed bootctrl hal error.

--- device.mk
+++ device.mk
@@ -49,8 +49,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     bootctrl.lahaina
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    
+PRODUCT_PACKAGES += \
     bootctrl.lahaina \
     libgptutils \
     libz \
